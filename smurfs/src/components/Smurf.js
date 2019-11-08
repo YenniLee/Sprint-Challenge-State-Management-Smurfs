721e21d49e102c88c40e3421db2be2c3146a18fa
Jennie Van <missjenvan@gmail.com>
2019-11-08 10:33:47 -0800

    delete functional

---
+++ smurfs/src/components/Smurf.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { deleteSmurf } from '../actions';
+import { connect } from 'react-redux'
+
+const Smurf = ({ smurf, deleteSmurf }) => {
+    
+    const handleDelete = () => {
+        deleteSmurf(smurf.id)
+    };
+
+    return (
+        <div className='smurf'>
+            <h3>Name: {smurf.name}</h3>
+            <p>Age: {smurf.age}</p>
+            <p>Height: {smurf.height}</p>
+            <button onClick={handleDelete}>Remove</button>
+        </div>
+    )
+};
+
+export default connect(null, { deleteSmurf })(Smurf);
--- smurfs/src/components/SmurfList.js
+++ smurfs/src/components/SmurfList.js
@@ -1,28 +1,23 @@
 import React, { useEffect } from "react";
+import Smurf from './Smurf';
 import { connect } from "react-redux";
-
-import { fetchSmurfs } from "../actions";
+import { fetchSmurfs, deleteSmurf } from "../actions";
 
 function SmurfList({ fetchSmurfs, isFetching, smurfs, error }) {
   useEffect(() => {
     fetchSmurfs();
   }, [fetchSmurfs]);
 
-  // const handleDelete = e => {
-  //   e.preventDefault();
-  //   deleteSmurf(smurfs.id)
-  // }
+  const handleDelete = e => {
+    e.preventDefault();
+    deleteSmurf(smurfs.id)
+  }
 
   return (
     <div className='smurfBox'>
       {smurfs.map(smurf => {
         return (
-          <div className='names' key={smurf.id}>
-            <h3>Name: {smurf.name}</h3>
-            <p>Age: {smurf.age}</p>
-            <p>Height: {smurf.height}</p>
-            {/* <button onClick={handleDelete}>Remove</button> */}
-          </div>
+          <Smurf key={smurf.id} smurf={smurf} />
         );
       })}
     </div>
@@ -39,5 +34,5 @@ const mapStateToProps = state => {
 
 export default connect(
   mapStateToProps,
-  { fetchSmurfs}
+  { fetchSmurfs, deleteSmurf}
 )(SmurfList);
